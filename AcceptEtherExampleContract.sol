874777fb61936811a69573bcd4d47ea1c651c5d8
Jemil <oyebisijemil@gmail.com>
2021-06-01 15:24:36 -0400

    ERC20Token conract has a mint function that increment the balance of the caller, it is called withing AcceptEtherExampleContract. The AcceptEtherExample was modified to use the external contract

--- AcceptEtherExampleContract.sol
+++ AcceptEtherExampleContract.sol
@@ -1,23 +1,25 @@
-//SDPX-License-Identifier: GLP-3.0
+//SDPX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16<0.9.0;
 
 contract AcceptEtherExampleContract {
     mapping(address=>uint) public _balances;
-    address payable _wallet;
+    address payable wallet;
+    address public tokenOftheExternalContract;
     
-    constructor(address payable wallet) public {
-        _wallet = wallet;
+    constructor(address payable _wallet, address _tokenOftheExternalContract) public {
+        wallet = _wallet;
+        tokenOftheExternalContract = _tokenOftheExternalContract;
     }
     
     event Purchase(address buyer, uint amount);
     
-    function() external payable {
+    function() external payable{
         buyToken();
     }
 
     function buyToken() public payable {
-        _balances[msg.sender] += 1;
-         _wallet.transfer(msg.value);
+         ERC20Token(address(tokenOftheExternalContract)).mint();
+         wallet.transfer(msg.value);
          emit Purchase(msg.sender,1);
     }
 }
