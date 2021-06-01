8ee7fe4db4e0448de439de28955d83f0a663fa96
Jemil <oyebisijemil@gmail.com>
2021-06-01 16:15:20 -0400

    Added a ChildContract that inherits from ERC20Token the mint function in the parent contract was changed to virtual and the was overriden by the child contract

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,11 +1,27 @@
- //SPDX-License-Identifier: GPL-3.0
- 
- pragma solidity >=0.4.16 <0.9.0;
+//SDPX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.16 <0.9.0;
  
  contract ERC20Token{
      mapping(address => uint256) public balances;
      
-     function mint() public {
+     function mint() public virtual {
          balances[tx.origin]++;
      }
  }
+
+contract ChildContract is ERC20Token {
+    string public symbol;
+    address[] public owners;
+    uint256 ownersCount;
+
+    constructor(string memory _name, string memory _symbol) ERC20Token(_name) public{
+        symbol = _symbol;
+    }
+
+    function mint() public override{
+        super.mint();
+        ownersCount++;
+        owners.push(msg.sender);
+    }
+}
