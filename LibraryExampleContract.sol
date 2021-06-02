c3b9aac31b61a2873b291cec2c2f1f06f5d68720
Jemil <oyebisijemil@gmail.com>
2021-06-02 18:01:46 -0400

    Added SafeMath library and used in a contract by making use of using SafeMath for uint256 declearation

--- LibraryExampleContract.sol
+++ LibraryExampleContract.sol
@@ -1,11 +1,12 @@
 //SDPX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.4.16 <0.9.0;
-import "./Math.sol";
+import "./SafeMath.sol";
 
 contract LibraryExampleContract{
+    using SafeMath for uint256;
     uint256 public value;
     function calculate(uint firstValue, uint secondValue) public{
-        value = Math.devide(firstValue,secondValue);
+        value = firstValue.div(secondValue);
     }
 }
